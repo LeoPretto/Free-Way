c492142b0ea778429afd384ea9516e532fc45a80
LeoPretto <leonardoquelve.oln@hotmail.com>
2022-09-30 10:43:21 -0300

    adicionando backgound da estrada

---
+++ sketch.js
@@ -0,0 +1,13 @@
+let imgEstrada;
+
+function preload(){
+    imgEstrada = loadImage("imagens_do_jogo/estrada.png")
+}
+
+function setup(){
+    createCanvas(500,400);
+}
+
+function draw(){
+    background(imgEstrada);
+}
